f00fd435ba46b17fd8af5758a307a53f788d3c4f
hsargeant <hssoftwareengineering@gmail.com>
2022-09-28 15:43:51 -0400

    google auth

--- config/passport.js
+++ config/passport.js
@@ -33,7 +33,7 @@ module.exports = function (passport) {
   passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:'/auth/google/callback'
+    callbackURL:'https://math-study-app-demo.herokuapp.com/auth/google/callback'
    },
    async(accessToken,refreshToken,profile,done)=>{ 
  
