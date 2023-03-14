30c708f362ede08113e6630755a364e681154032
hsargeant <hssoftwareengineering@gmail.com>
2023-03-14 10:16:33 -0400

    google button

--- client/src/pages/Index.js
+++ client/src/pages/Index.js
@@ -11,15 +11,15 @@ export default function Index() {
                         <p className="mt-3 text-base text-gray-900 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">Mindful Math is an online study aid that allows you to keep track of all your assignments, class notes, create and study flashcards, and gain access to resources from all over the web. Mindful Math will help you organize your academic life, ensuring that you never miss a piece of homework, or forget a lesson.</p>
                         <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                             <div className="rounded-md shadow">
-                              <Link to="/login" >
-                               <a className="flex w-full items-center justify-center rounded-md border border-transparent bg-blue-600 px-8 py-3 text-base font-medium text-white hover:bg-blue-700 md:py-4 md:px-10 md:text-lg">Log In</a>
+                              <Link to="/login" className="flex w-full items-center justify-center rounded-md border border-transparent bg-blue-600 px-8 py-3 text-base font-medium text-white hover:bg-blue-700 md:py-4 md:px-10 md:text-lg">
+                               Log In
                                 </Link>
                             </div>
                             <div className="mt-3 sm:mt-0 sm:ml-3">
-                              <Link to="/signup">
-                                <a className="flex w-full items-center justify-center rounded-md border border-transparent bg-gray-500 px-8 py-3 text-base font-medium text-white hover:bg-gray-600 md:py-4 md:px-10 md:text-lg">
+                              <Link to="/signup" className="flex w-full items-center justify-center rounded-md border border-transparent bg-gray-500 px-8 py-3 text-base font-medium text-white hover:bg-gray-600 md:py-4 md:px-10 md:text-lg">
+                              
                                 Sign Up
-                                </a>
+                                
                                 </Link>
                             </div>
                           </div>
