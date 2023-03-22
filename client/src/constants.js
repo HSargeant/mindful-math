1319e94840550c0bc6f89ea5ecb2af208d4c1f62
hsargeant <hssoftwareengineering@gmail.com>
2023-03-22 03:52:42 -0400

    update production route

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE = process.env.NODE_ENV =='production' ? "http://localhost:8000" :'http://localhost:8000'
+export const API_BASE = process.env.NODE_ENV =='production' ? "https://mindful-math.cyclic.app" :'http://localhost:8000'
 // console.log(API_BASE)
 
 export const links = {
