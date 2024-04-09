e3fa1a581e6e1839d034aaeaffe6e46e3ff3aa23
hsargeant <hssoftwareengineering@gmail.com>
2024-04-09 17:37:02 -0400

    add react query update auth

--- client/src/pages/Root.js
+++ client/src/pages/Root.js
@@ -1,18 +1,6 @@
-import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
-import { API_BASE } from "../constants";
 export default function Root(props) {
-  const [user, setUser] = useState();
-  // const [messages, setMessages] = useState()
-  useEffect(() => {
-    const getUser = async () => {
-        const res = await fetch(API_BASE + "/api/user", { credentials: "include" })
-        const data = await res.json()
-        setUser(data.user)
-    }
-    getUser()
-  }, []);
   return (
-    <Outlet context={{user}}/>
+    <Outlet/>
   );
 }
